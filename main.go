2cc6b5b68d15559e62a3a77f83b7c9b2cff3dd59
kyler-swanson <kylerswanson@gmail.com>
2023-05-06 21:08:36 -0400

    feat: web server hookup

--- main.go
+++ main.go
@@ -1,9 +1,63 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/kyler-swanson/truncr/db"
+	"github.com/kyler-swanson/truncr/handler"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	addr := ":8080"
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("err: %s", err.Error())
+	}
+
+	dbUser, dbPassword, dbName :=
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB")
+	database, err := db.Initialize(dbUser, dbPassword, dbName)
+	if err != nil {
+		log.Fatalf("err db: %v", err)
+	}
+
+	defer database.Conn.Close()
+
+	httpHandler := handler.CreateHandler(database)
+	server := &http.Server{
+		Handler: httpHandler,
+	}
+
+	go func() {
+		server.Serve(listener)
+	}()
+
+	defer Stop(server)
+
+	log.Printf("started truncr API on %s", addr)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(fmt.Sprint(<-ch))
+
+	log.Println("stopping API server...")
+}
+func Stop(server *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("could not shut down server: %v\n", err)
+		os.Exit(1)
+	}
 }
